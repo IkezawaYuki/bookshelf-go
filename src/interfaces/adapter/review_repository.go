61a9756b190fbe2dcc470887094ece3e5cb8117c
yikezawa <y.ikezawa93@gmail.com>
2021-05-11 20:57:39 +0900

    mod

--- src/interfaces/adapter/review_repository.go
+++ src/interfaces/adapter/review_repository.go
@@ -60,25 +60,62 @@ func (r *reviewRepository) FindReviewByID(id int) (review entity.Review, err err
 }
 
 func (r *reviewRepository) getCreateReviewQuery() string {
-
+	return `INSERT INTO reviews (book_id, content, reading_date, create_user_id) VALUES (?, ?, ?, ?)`
 }
 
 func (r *reviewRepository) CreateReview(userID int, review entity.Review) error {
-	panic("implement me")
+	query := r.getCreateReviewQuery()
+	_, err := r.handler.Exec(query,
+		review.BookID,
+		review.Content,
+		review.ReadingDate,
+		userID,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *reviewRepository) getUpdateReviewQuery() string {
-
+	return `UPDATE reviews SET
+book_id = ?,
+content = ?,
+reading_date = ?,
+create_user_id = ?
+WHERE id = ?`
 }
 
 func (r *reviewRepository) UpdateReview(userID int, review entity.Review) error {
-	panic("implement me")
+	query := r.getUpdateReviewQuery()
+	_, err := r.handler.Exec(query,
+		review.BookID,
+		review.Content,
+		review.ReadingDate,
+		userID,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (r *reviewRepository) getDeleteReviewByIDQuery() string {
-
+	return `UPDATE reviews
+SET delete_user_id = ?,
+delete_date = now(),
+delete_flag = 1
+WHERE id = ?`
 }
 
 func (r *reviewRepository) DeleteReviewByID(userID int, id int) error {
-	panic("implement me")
+	query := r.getDeleteReviewByIDQuery()
+	_, err := r.handler.Exec(query,
+		userID,
+		id,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
