172eb729d5d2c7d01115f2ad344c6a04043cc2bf
yikezawa <y.ikezawa93@gmail.com>
2021-05-24 00:02:42 +0900

    add log

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/IkezawaYuki/bookshelf-go
 go 1.16
 
 require (
-	github.com/go-redis/redis/v8 v8.8.2 // indirect
+	github.com/go-redis/redis/v8 v8.8.2
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/sarulabs/di v2.0.0+incompatible // indirect
+	github.com/sarulabs/di v2.0.0+incompatible
+	github.com/sirupsen/logrus v1.8.1
 )
--- src/logger/logger.go
+++ src/logger/logger.go
@@ -1,1 +1,55 @@
 package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"strings"
+)
+
+var (
+	Log *logrus.Logger
+)
+
+func init() {
+	level, err := logrus.ParseLevel("DEBUG")
+	if err != nil {
+		level = logrus.DebugLevel
+	}
+	Log = &logrus.Logger{
+		Out:       os.Stdout,
+		Formatter: &logrus.JSONFormatter{},
+		Level:     level,
+	}
+}
+
+func Info(msg string, tags ...string) {
+	if Log.Level < logrus.InfoLevel {
+		return
+	}
+	Log.WithFields(parseFields(tags...)).Info(msg)
+}
+
+func Debug(msg string, tags ...string) {
+	if Log.Level < logrus.DebugLevel {
+		return
+	}
+	Log.WithFields(parseFields(tags...)).Debug(msg)
+}
+
+func Error(msg string, err error, tags ...string) {
+	if Log.Level < logrus.ErrorLevel {
+		return
+	}
+	msg = fmt.Sprintf("%s - ERROR - %v", msg, err)
+	Log.WithFields(parseFields(tags...)).Error(msg)
+}
+
+func parseFields(tags ...string) logrus.Fields {
+	result := make(logrus.Fields, len(tags))
+	for _, tags := range tags {
+		els := strings.Split(tags, ":")
+		result[strings.TrimSpace(els[0])] = strings.TrimSpace(els[1])
+	}
+	return result
+}
