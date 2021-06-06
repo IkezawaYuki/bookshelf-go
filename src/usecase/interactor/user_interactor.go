01a1f74439c176ac542c2795cb52bc975cfb2a86
yikezawa <y.ikezawa93@gmail.com>
2021-06-06 12:53:34 +0900

    mod

--- src/usecase/interactor/user_interactor.go
+++ src/usecase/interactor/user_interactor.go
@@ -2,37 +2,40 @@ package interactor
 
 import (
 	"github.com/IkezawaYuki/bookshelf-go/src/domain/entity"
-	"github.com/IkezawaYuki/bookshelf-go/src/interfaces/adapter"
+	"github.com/IkezawaYuki/bookshelf-go/src/domain/repository"
 	"github.com/IkezawaYuki/bookshelf-go/src/usecase/inputport"
 )
 
 type userInteractor struct {
+	userRepo repository.UserRepository
 }
 
-func NewUserInteractor() inputport.UserInputPort {
-	return &userInteractor{}
+func NewUserInteractor(repo repository.UserRepository) inputport.UserInputPort {
+	return &userInteractor{
+		userRepo: repo,
+	}
 }
 
 func (u *userInteractor) FindAllUser() (entity.Users, error) {
-	panic("implement me")
+	return u.userRepo.FindAllUser()
 }
 
 func (u *userInteractor) FindUserByID(id int) (*entity.User, error) {
-	panic("implement me")
+	return u.userRepo.FindUserByID(id)
 }
 
 func (u *userInteractor) CreateUser(userID int, user entity.User) (*entity.User, error) {
-	panic("implement me")
+	return u.userRepo.CreateUser(userID, user)
 }
 
 func (u *userInteractor) UpdateUser(userID int, user entity.User) error {
-	panic("implement me")
+	return u.userRepo.UpdateUser(userID, user)
 }
 
 func (u *userInteractor) DeleteUserByID(userID int, id int) error {
-	panic("implement me")
+	return u.userRepo.DeleteUserByID(userID, id)
 }
 
 func (u *userInteractor) FindUserByEmail(email string) (*entity.User, error) {
-	return adapter.UserRepo.FindUserByEmail(email)
+	return u.userRepo.FindUserByEmail(email)
 }
